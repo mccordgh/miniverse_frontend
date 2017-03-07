c3bd398e4d9f3a2b854313f328c11150c360acb4
Paul Martin <Dmart331@gmail.com>
2017-03-07 10:16:40 -0600

    Added product categories

--- app/app.js
+++ app/app.js
@@ -17,6 +17,10 @@ app.config(($locationProvider, $routeProvider) => {
       templateUrl: 'app/partials/cart.html',
       controller: 'cartCtrl'
     })
+    .when('/products',{
+      templateUrl: 'app/partials/product_list.html',
+      controller: 'productListCtrl'
+    })
     .when('/product-detail', {
       templateUrl: 'app/partials/product_detail.html',
       controller: 'productDetailCtrl'
