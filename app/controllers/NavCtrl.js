4592e2afd933506736b38655e6f091a4fa8e56ac
Nick Chemsak <nchemsak@gmail.com>
2017-03-08 10:23:29 -0600

    login

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,5 +1,5 @@
 'use strict';
-app.controller('NavCtrl', function($scope) {
+app.controller('NavCtrl', function($scope, $http, $location) {
   $scope.navItems = [
     { name: "Home", url: '#/home' },
     { name: "Products", url: '#/products' },
